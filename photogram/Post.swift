4c946830d595006779e8848f4ec804f81201828a
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-05-28 00:47:32 -0400

    added basic models

---
+++ photogram/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  photogram
+//
+//  Created by Gabriele on 5/28/17.
+//  Copyright © 2017 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    var imageURL = ""
+    var imageDescription = ""
+    var numberOfLikes = 0
+    var authorEmail = ""
+    var uuid = ""
+    var key = ""
+}
---
+++ photogram/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  photogram
+//
+//  Created by Gabriele on 5/28/17.
+//  Copyright © 2017 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
