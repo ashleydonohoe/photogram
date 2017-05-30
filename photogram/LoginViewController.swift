23cd98c793cca020d2e289804e450f85755b6f9d
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-05-30 00:59:32 -0400

    login and signup setup

--- photogram/LoginViewController.swift
+++ photogram/LoginViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class LoginViewController: UIViewController {
 
@@ -21,16 +24,45 @@ class LoginViewController: UIViewController {
     @IBAction func loginPressed(_ sender: Any) {
         // Check for complete user info
         if emailTextField.text != "" && passwordTextField.text != "" {
-            
-            
-            performSegue(withIdentifier: "loginSegue", sender: nil)
-            
+            // If the user has an account
+            FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+                // If error, show the error as an alert if the error is not FIRAuthErrorCodeUserNotFound
+                if error != nil {
+                    // If error is FIRAuthErrorCodeUserNotFound
+                    if(error!.localizedDescription.contains("no user record")) {
+                        // Try to create an account
+                        FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                            if error != nil {
+                                // show error message
+                                self.showAlert(alertTitle: "Error", message: "Error creating account", actionTitle: "OK")
+                            } else {
+                                // Add user to database
+                                let users = FIRDatabase.database().reference().child("users")
+                                users.child(user!.uid).child("email").setValue(user!.email)
+                                // continue into app
+                                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                            }
+                        })
+                    } else {
+                        // Else show error
+                        self.showAlert(alertTitle: "Error", message: "Could not log in or create account", actionTitle: "OK")
+                    }
+                } else {
+                    // If no password error or network error, they're logged in and continue into the app
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+
+                }
+            })
         } else {
             // Show error if not present
-            let alert = UIAlertController(title: "Error", message: "Both email and password required", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alert.addAction(okAction)
-            self.present(alert, animated: true, completion: nil)
+            showAlert(alertTitle: "Error", message: "Both username and password required", actionTitle: "OK")
         }
+    }
+    
+    func showAlert(alertTitle: String, message: String, actionTitle: String) {
+        let alert = UIAlertController(title: alertTitle, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .cancel, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
 }
