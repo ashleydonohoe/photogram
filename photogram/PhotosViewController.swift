5257e4d538c86868fa053afab4c18cb4b7bab301
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-05-30 01:30:43 -0400

    fixed signout. added bar buttons

--- photogram/PhotosViewController.swift
+++ photogram/PhotosViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tabBarController?.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logoutPressed(_:)))
+        tabBarController?.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "📷", style: .plain, target: self, action: #selector(addPhotoPressed(_:)))
+
     }
     
-    @IBAction func logoutPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+ @IBAction func logoutPressed(_ sender: Any) {
+        do {
+            try FIRAuth.auth()?.signOut()
+            dismiss(animated: true, completion: nil)
+        } catch {
+           print("Error signing out")
+        }
+    }
+
+    @IBAction func addPhotoPressed(_ sender: Any) {
+        print("Add photo")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
